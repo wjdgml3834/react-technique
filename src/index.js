79516bf284c619aabee7ff6e850dfda11345091e
junghee.kim <wjdgml3834@naver.com>
2022-01-18 04:10:20 +0900

    📝  ReactDom.render란 무엇인가

--- src/index.js
+++ src/index.js
@@ -16,6 +16,10 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+// 이 코드는 컴포넌트를 페이지에 렌더링 하는 역할을 한다.  react-dom 모듈을 불러와 사용할 수 있습니다.
+// 이 함수의 첫 번째 파라미터에는 페이지에 렌더링할 내용을 JSX 형태로 작성하고,
+// 두 번째 파라미터에는 해당 JSX를 렌더링할 document 내부 요소를 설정합니다.
+// 여기서는 id가 root인 요소 안에 렌더링을 하도록 설정했는데요. 이 요소는 public/index.html 파일을 열어 보면 있답니다.
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
