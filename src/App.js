d20c4b8816c471a5f1de1a2ad3a9accb02917468
junghee.kim <wjdgml3834@naver.com>
2022-01-18 03:32:17 +0900

    📝  웹팩이 로드할 수 있는 것들

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// 웹팩을 사용하면 SVG 파일과 CSS파일도 불러와서 사용할 수 있다.
+// 이렇게 파일들을 불러오는 것은 웹팩의 로더(loader)라는 기능이 담당한다.
+// 예를들어 css-loader는 CSS파일을 불러올수 있게 해주고
+// file-loader는 웹 폰트나 미디어 파일 등을 불러올 수 있게 해준다.
+// 그리고 babel-loader는 자바스크립트 파일을 불러오면서 최신 자바스크립트 문법으로 작성된 코드를
+// 바벨이라는 도구를 사용하여 ES5문법으로 변환해준다.
+// 우리가 앞으로 배우게 될 리액트 컴포넌트에서 사용하는 JSX라는 문법도 정식 자바스크립트 문법이 아니므로
+// ES5 형태의 코드로 변환해야 합니다.
 
 function App() {
   return (
