b3f3e48915d8e2df14b002e3277c2543470f0f46
junghee.kim <wjdgml3834@naver.com>
2022-01-24 06:07:53 +0900

    ✨  6.4.2 데이터 추가 기능 구현

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -11,12 +11,37 @@ const IterationSample = () => {
   //   텍스트 입력할 수 있는 input의 상태
   const [userInp, setUserInp] = useState("");
   // 데이터 배열에서 새로운 항목을 추가할 때 사용할 고유 id를 위한 상태
+  //   4개까지 만들었으니 다음 id가 5부터 오게 설정
   const [nextId, setNextId] = useState(5);
+
+  //  input의 value를 업데이트
+  const onChange = (event) => {
+    setUserInp(event.target.value);
+  };
+  const onClick = () => {
+    //   concat을 사용하여 새로운 항목을 추가한 배열을 만들고, setBrandName을 통해 상태 업데이트
+    const nextBrand = brandName.concat({
+      id: nextId,
+      //   nextId 값을 id로 설정
+      brand: userInp,
+    });
+    // nextId를 1증가시킨다.
+    setNextId((current) => current + 1);
+    setBrandName(nextBrand);
+    // input을 비운다.
+    setUserInp("");
+  };
 
   const brandList = brandName.map((item) => (
     <li key={item.id}>{item.brand}</li>
   ));
-  return <ul>{brandList}</ul>;
+  return (
+    <>
+      <input value={userInp} onChange={onChange} />
+      <button onClick={onClick}>브랜드 추가</button>
+      <ul>{brandList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
