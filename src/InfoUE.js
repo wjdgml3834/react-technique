745480ff0deee51a19b2e056d89ce1aeaaf4d951
junghee.kim <wjdgml3834@naver.com>
2022-01-26 07:55:10 +0900

    ✨  8.2.1 예제 useEffect 마운트 될때만 실행

--- src/InfoUE.js
+++ src/InfoUE.js
@@ -5,12 +5,17 @@ const InfoUE = () => {
   const [singerName, setSingerName] = useState("");
 
   useEffect(() => {
-    console.log("렌더링이 완료되었음! ");
-    console.log({
-      fruitName,
-      singerName,
-    });
-  });
+    console.log("마운트 될때만 실행");
+  }, []);
+  // 컴포넌트가 화면에 처음 렌더링 될 때만 실행하고, 업데이트 때는 실행하지 않는다.
+
+  // useEffect(() => {
+  //   console.log("렌더링이 완료되었습니다!");
+  //   console.log({
+  //     fruitName,
+  //     singerName,
+  //   });
+  // });
   // 컴포넌트가 리렌더링 될 때마다 특정 작업 수행
 
   const onChangeFruitName = (event) => {
