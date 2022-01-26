bebe8c28711a7b8be6af675f54c1c6143ad3f193
junghee.kim <wjdgml3834@naver.com>
2022-01-26 09:08:06 +0900

    ✨  8.2.3 예제 보이기/숨기기 버튼

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Counter from "./Counter";
 import Info from "./Info";
 import InfoUE from "./InfoUE";
+import InfoVisible from "./InfoVisible";
 
 function App() {
-  return <InfoUE />;
+  return <InfoVisible />;
 }
 
 export default App;
