d9a77155298cf88fa01f10fb90f0f51482144286
junghee.kim <wjdgml3834@naver.com>
2022-02-06 15:42:23 +0900

    🐛  경로 겹치면 컴포넌트 동시 렌더링 문제 해결

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from "./About";
 function App() {
   return (
     <div>
-      <Route path="/" component={Home} />
+      <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
     </div>
   );
@@ -15,3 +15,5 @@ export default App;
 
 // Route라는 컴포넌트를 사용하여 사용자의 현재 경로에 따라 다른 컴포넌트를 보여준다.
 // 사용 방식은 <Route path="주소규칙" component={보여 줄 컴포넌트 } />
+// 문제: "/"가 서로 겹치기 때문에 두 컴포넌트가 두개 다 모두 나타난다.
+// exact={true}를 설정하면 해결된다.
