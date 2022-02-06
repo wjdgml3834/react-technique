4e5b15e8469de25c3970f99f599fa0128f40a799
junghee.kim <wjdgml3834@naver.com>
2022-02-06 15:50:32 +0900

    ✨  13.2.5 Link 컴포넌트 사용하여 다른 주소로 이동

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
     </div>
@@ -13,7 +22,6 @@ function App() {
 
 export default App;
 
-// Route라는 컴포넌트를 사용하여 사용자의 현재 경로에 따라 다른 컴포넌트를 보여준다.
-// 사용 방식은 <Route path="주소규칙" component={보여 줄 컴포넌트 } />
-// 문제: "/"가 서로 겹치기 때문에 두 컴포넌트가 두개 다 모두 나타난다.
-// exact={true}를 설정하면 해결된다.
+// Link 컴포넌트를 사용하여 다른 주소로 이동
+// a태그와 달리 페이지 주소만 변경해주고, 페이지를 새로 불러오지는 않는다.
+// 사용법: <Link to="주소">내용 </Link>
