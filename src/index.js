8b7ddd6b4227d13dbd80929be51178aed7ba5561
junghee.kim <wjdgml3834@naver.com>
2022-02-06 15:18:49 +0900

    ✨  13.2.2 프로젝트에 라우터 적용

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById("root")
 );
+
+// BrowserRouter는 웹 애플리케이션에 HTML5의 History API를 사용하여 페이지를 새로고침하지 않고도 주소를 변경하고
+// 현재 주소에 관련된 정보를 props로 쉽게 조회하거나 사용할 수 있도록 해준다.
